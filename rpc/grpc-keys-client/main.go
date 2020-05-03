8dab2fa81017c2154d1d32d172020787450a064f
JosephChan007 <zjzyp007@hotmail.com>
2020-05-03 22:43:34 +0800

    commit

---
+++ rpc/grpc-keys-client/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"github.com/JosephChan007/go-rpc/rpc/message"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"log"
+	"time"
+)
+
+func main() {
+
+	creds, err := credentials.NewClientTLSFromFile("keys-client/server.crt", "hdfs-host3")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	conn, err := grpc.Dial("hdfs-host3:9091", grpc.WithTransportCredentials(creds))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	c := message.NewOrderServiceClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	r, err := c.GetOrder(ctx, &message.OrderRequest{
+		OrderId:   "20200501002",
+		Timestamp: 0,
+	})
+	if err != nil {
+		log.Fatalf("Order client could not invoke: %v", err)
+	}
+	log.Printf("Order info is: %v", r)
+}
---
+++ rpc/grpc-keys-server/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/JosephChan007/go-rpc/rpc/message"
+	"github.com/JosephChan007/go-rpc/rpc/service"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"log"
+	"net"
+)
+
+func main() {
+
+	// 单向数字签名证书
+	cerds, err := credentials.NewServerTLSFromFile("keys-server/server.crt", "keys-server/server_no_password.key")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 用证书建立服务端
+	grpcServer := grpc.NewServer(grpc.Creds(cerds))
+	message.RegisterOrderServiceServer(grpcServer, new(service.OrderServiceImpl))
+
+	lis, err := net.Listen("tcp", ":9091")
+	if err != nil {
+		log.Fatalf("Order service failed to listen: %v", err)
+	}
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Order service failed to serve: %v", err)
+	}
+}
