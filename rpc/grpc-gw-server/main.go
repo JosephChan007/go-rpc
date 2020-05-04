4f6c021947bac5d9c4f4284af984a3aeab76e6c5
JosephChan007 <zjzyp007@hotmail.com>
2020-05-04 18:16:59 +0800

    commit

--- rpc/grpc-gw-server/main.go
+++ rpc/grpc-gw-server/main.go
@@ -1,28 +1,35 @@
 package main
 
-import (
-	"github.com/JosephChan007/go-rpc/rpc/message"
-	"github.com/JosephChan007/go-rpc/rpc/service"
-	"github.com/JosephChan007/go-rpc/rpc/util"
-	"google.golang.org/grpc"
-	"log"
-	"net"
-)
-
 func main() {
+	/*
+		// 以http形式对外提供接口
+		// 由于以下gw文件创建命令报错，故将代码注释
+		// protoc --grpc-gateway_out=logtostderr=true:. message.proto
 
-	// 双向数字签名证书
-	cred := util.ServerCertHelper()
+		// 双向数字签名证书
+		cred := util.ServerCertHelper()
 
-	// 用证书建立服务端
-	grpcServer := grpc.NewServer(grpc.Creds(cred))
-	message.RegisterOrderServiceServer(grpcServer, new(service.OrderServiceImpl))
+		muxServer := runtime.NewServeMux()
 
-	lis, err := net.Listen("tcp", ":9091")
-	if err != nil {
-		log.Fatalf("Order service failed to listen: %v", err)
-	}
-	if err := grpcServer.Serve(lis); err != nil {
-		log.Fatalf("Order service failed to serve: %v", err)
-	}
+		option := grpc.DialOption(grpc.WithTransportCredentials(cred))
+		err := message.RegisterOrderServiceHandlerFromEndpoint(
+			context.Background(),
+			muxServer,
+
+			// 注意: 此处必须用grpc服务端所在地址，若http接口服务端与grpc服务端部署在同一台机器，可以用:localhost:9090
+			"hdfs-host3:9090",
+
+			option
+		)
+		if err != nil {
+			log.Fatalf("did not connect: %v", err)
+		}
+
+		httpServer := &http.Server{
+			Addr:    ":9091",	// 此处是http接口的端口
+			Handler: muxServer,
+		}
+		httpServer.ListenAndServe()
+
+	*/
 }
