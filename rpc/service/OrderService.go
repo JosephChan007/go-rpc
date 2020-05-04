bb44d69e223a357232b7f7ff06d10e1d8f829836
JosephChan007 <zjzyp007@hotmail.com>
2020-05-04 21:58:00 +0800

    commit

--- rpc/service/OrderService.go
+++ rpc/service/OrderService.go
@@ -5,16 +5,15 @@ import (
 	"errors"
 	"fmt"
 	"github.com/JosephChan007/go-rpc/rpc/message"
-	"log"
 )
 
-func OrderHouse() *map[string]message.OrderInfo {
-	orderMap := make(map[string]message.OrderInfo, 5)
-	orderMap["1"] = message.OrderInfo{OrderId: "20200501001", OrderName: "橙子", OrderStatus: "待付款"}
-	orderMap["2"] = message.OrderInfo{OrderId: "20200501002", OrderName: "苹果", OrderStatus: "已付款"}
-	orderMap["3"] = message.OrderInfo{OrderId: "20200501003", OrderName: "梨", OrderStatus: "已提交"}
-	orderMap["4"] = message.OrderInfo{OrderId: "20200501004", OrderName: "香蕉", OrderStatus: "退款中"}
-	orderMap["5"] = message.OrderInfo{OrderId: "20200501005", OrderName: "火龙果", OrderStatus: "已退款"}
+func OrderHouse() *map[string]*message.OrderInfo {
+	orderMap := make(map[string]*message.OrderInfo, 5)
+	orderMap["1"] = &message.OrderInfo{OrderId: "20200501001", OrderName: "橙子", OrderStatus: "待付款"}
+	orderMap["2"] = &message.OrderInfo{OrderId: "20200501002", OrderName: "苹果", OrderStatus: "已付款"}
+	orderMap["3"] = &message.OrderInfo{OrderId: "20200501003", OrderName: "梨", OrderStatus: "已提交"}
+	orderMap["4"] = &message.OrderInfo{OrderId: "20200501004", OrderName: "香蕉", OrderStatus: "退款中"}
+	orderMap["5"] = &message.OrderInfo{OrderId: "20200501005", OrderName: "火龙果", OrderStatus: "已退款"}
 	return &orderMap
 }
 
@@ -54,7 +53,7 @@ func (s *OrderServiceImpl) GetOrder(ctx context.Context, req *message.OrderReque
 	orderMap := OrderHouse()
 	for _, v := range *orderMap {
 		if v.OrderId == req.OrderId {
-			res = &v
+			res = v
 			fmt.Printf("order service info is: %v\n", res)
 			break
 		}
@@ -72,19 +71,11 @@ func (s *OrderServiceImpl) GetOrderList(ctx context.Context, req *message.OrderS
 	fmt.Printf("order list service requst is: %v\n", req)
 
 	orderMap := OrderHouse()
-	od1 := (*orderMap)["1"]
-	od2 := (*orderMap)["3"]
-	od3 := (*orderMap)["4"]
-
-	log.Printf("Order info is: %v", od1)
-	log.Printf("Order info is: %v", od2)
-	log.Printf("Order info is: %v", od3)
-
 	return &message.OrderInfoList{
 		Orders: []*message.OrderInfo{
-			&od1,
-			&od2,
-			&od3,
+			(*orderMap)["1"],
+			(*orderMap)["3"],
+			(*orderMap)["5"],
 		},
 	}, nil
 }
